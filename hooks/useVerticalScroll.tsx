92134abc05b7613f68129c607e443eb8b991e5c7
Shivraj Nag <43899907+Shivraj97@users.noreply.github.com>
2022-05-17 09:33:56 +0530

    completed banner component

---
+++ hooks/useVerticalScroll.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+const useVerticalScroll = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+  return [isScrolled]
+}
+
+export default useVerticalScroll
