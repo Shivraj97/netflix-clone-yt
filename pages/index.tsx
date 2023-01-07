f2ed0a44b141b767d125de2cd1f2bc3018e74fb8
Shivraj Nag <43899907+Shivraj97@users.noreply.github.com>
2023-01-07 11:00:43 +0530

    completed homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Banner from '../components/Banner';
 import Header from '../components/Header';
 import { Movie } from '../typings';
 import requests from '../utils/requests';
+import Row from '../components/Row';
 
 interface Props {
   netflixOriginals: Movie[];
@@ -27,7 +28,6 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
-  console.log('netflixOriginals', netflixOriginals);
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -37,12 +37,17 @@ const Home = ({
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
